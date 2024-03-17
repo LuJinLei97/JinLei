0e749916a249a5ecf62391c44a9f160fc14dde0f
LuJinLei97 <106680800+LuJinLei97@users.noreply.github.com>
2024-03-17 11:28:34 +0800

    ConsoleUtility更新

--- src/JinLei/Utilities/ConsoleUtility.cs
+++ src/JinLei/Utilities/ConsoleUtility.cs
@@ -5,9 +5,11 @@ using JinLei.Extensions;
 namespace JinLei.Utilities;
 public static partial class ConsoleUtility
 {
-    public static string ReadPath(string tip = "Path:") => tip.Do(Console.Write).Return(Console.ReadLine().Trim('"'));
+    public static string TipAndReadLine(string tip = "Tip:") => (tip ?? string.Empty).Do(Console.Write).Return(Console.ReadLine());
 
-    public static DirectoryInfo ReadDirectoryPath(string tip = "Path:") => new(ReadPath(tip));
+    public static string ReadPath(string tip = "Path:") => TipAndReadLine(tip).Trim('"');
 
-    public static FileInfo ReadFilePath(string tip = "Path:") => new(ReadPath(tip));
+    public static DirectoryInfo ReadDirectoryPath(string tip = "DirectoryPath:") => new(ReadPath(tip));
+
+    public static FileInfo ReadFilePath(string tip = "FilePath:") => new(ReadPath(tip));
 }
