cb55567aaf693769bd02f499843a33e2eb7705c5
LuJinLei97 <106680800+LuJinLei97@users.noreply.github.com>
2024-03-17 11:23:31 +0800

    Revert "ConsoleUtility更新"
    This reverts commit bf9c6d7f6992f1541ecbce3aed4a1c6ad1a62fe4.

--- src/JinLei/Utilities/ConsoleUtility.cs
+++ src/JinLei/Utilities/ConsoleUtility.cs
@@ -5,11 +5,9 @@ using JinLei.Extensions;
 namespace JinLei.Utilities;
 public static partial class ConsoleUtility
 {
-    public static string TipAndReadLine(string tip = "tip:") => (tip ?? string.Empty).Do(Console.Write).Return(Console.ReadLine());
+    public static string ReadPath(string tip = "Path:") => tip.Do(Console.Write).Return(Console.ReadLine().Trim('"'));
 
-    public static string ReadPath(string tip = "Path:") => TipAndReadLine(tip).Trim('"');
+    public static DirectoryInfo ReadDirectoryPath(string tip = "Path:") => new(ReadPath(tip));
 
-    public static DirectoryInfo ReadDirectoryPath(string tip = "DirectoryPath:") => new(ReadPath(tip));
-
-    public static FileInfo ReadFilePath(string tip = "FilePath:") => new(ReadPath(tip));
+    public static FileInfo ReadFilePath(string tip = "Path:") => new(ReadPath(tip));
 }
